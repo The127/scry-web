84c0bc5281cd217f15b6cecd0567f5cb38130316
karo <karolin.kostial@gmail.com>
2025-05-16 23:48:33 +0200

    chore: add nix flake for dev env

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Elm Web UI";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in pkgs.mkShell {
+      buildInputs = [
+        pkgs.elmPackages.elm
+        pkgs.elmPackages.elm-format
+        pkgs.elmPackages.elm-live
+      ];
+    };
+  };
+}
